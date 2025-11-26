521754d9dc89d813999cd4585b9903ac7311d1b2
mercedes223mdp <msbichweiler@gmail.com>
2025-11-26 09:36:19 +0100

    paint array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import "../styles/App.scss";
+import { useState } from "react";
 
 const inicialCountries = [
   {
@@ -109,17 +110,87 @@ const inicialCountries = [
     ],
     "flag": "🇧🇿"
   },
+   {
+    "name": {
+      "common": "Belarus",
+      "official": "Republic of Belarus",
+      "nativeName": {
+        "bel": {
+          "official": "Рэспубліка Беларусь",
+          "common": "Белару́сь"
+        },
+        "rus": {
+          "official": "Республика Беларусь",
+          "common": "Беларусь"
+        }
+      }
+    },
+    "capital": [
+      "Minsk"
+    ],
+    "flag": "🇧🇾"
+  },
+  {
+    "name": {
+      "common": "Mauritius",
+      "official": "Republic of Mauritius",
+      "nativeName": {
+        "eng": {
+          "official": "Republic of Mauritius",
+          "common": "Mauritius"
+        },
+        "fra": {
+          "official": "République de Maurice",
+          "common": "Maurice"
+        },
+        "mfe": {
+          "official": "Republik Moris",
+          "common": "Moris"
+        }
+      }
+    },
+    "capital": [
+      "Port Louis"
+    ],
+    "flag": "🇲🇺"
+  },
 ];
 
 
 function App() {
+
+  const [countries, setCountries] = useState(inicialCountries);
+
+  const [filterByName, setFilterByName] = useState("");
+
+
   return (
     <div>
       <header className="header">
         <h1 className="title">Country Info App</h1>
         <p className="title">Explore information about countries, capitals, and flags. Add new countries and filter through the list.</p>
       </header>
-      <main className="main"></main>
+      <main className="main">
+        <section>
+          <p>Filters</p>
+          <form className="form-filters">
+            <label htmlFor="filter-country">By Country</label>
+            <input type="text" name="filter-country" id="filter-country" className="input" placeholder="Belize"/>
+            <label htmlFor="filter-continent">By Continent</label>
+          </form>
+        </section>
+        <section>
+          <ul className="country-list">
+            {countries.map((country, index) => (
+              <li key={index} className="country-item">
+                <h2 className="country-name">{country.name.common}</h2>
+                <p className="country-capital">Capital: {country.capital[0]}</p>
+                <p className="country-flag">Flag: {country.flag}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
